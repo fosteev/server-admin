022372c656043814854cb2137fd4979dbb15cc97
Unknown <fosteev3@gmail.com>
2019-01-02 09:21:16 -0500

    Add fetch class

--- src/actions/сonfig.js
+++ src/actions/сonfig.js
@@ -29,7 +29,7 @@ export function stringParams(params) {
         arr.push(`${key}=${params[key]}`)
     }
 
-    if  (arr.length === 0) {
+    if (arr.length === 0) {
         return '';
     }
 
@@ -63,4 +63,59 @@ export function fetchRequest(dispatch, url, params) {
                 //return dispatch(onShowFailResponseFromServer());
             })
     })
+}
+
+import {setMessageBox} from './state';
+
+export class Fetch {
+    constructor(url) {
+        this.url = url;
+    }
+
+    setUrl(url) {
+        this.url = url;
+    }
+
+    getUrl() {
+        return this.url;
+    }
+
+    paramsToString(params) {
+        let arr = [];
+
+        for (const key in params) {
+            arr.push(`${key}=${params[key]}`)
+        }
+
+        if (arr.length === 0) {
+            return '';
+        }
+
+        return `?${arr.join('&')}`;
+    }
+
+    request(route, method, params) {
+        const url = `${this.url}${route}${this.paramsToString(params)}`;
+        const options = {
+            method: method,
+            credentials: 'include'
+        };
+
+        return new Promise((resolve, reject) => {
+            fetch(url, options).then((res) => {
+                switch (res.status) {
+                    case 404:
+                        reject({
+                            status: 404,
+                            text: 'Not found page'
+                        });
+                }
+
+                res.json()
+                    .then(json => resolve(json))
+                    .catch(error => reject(error));
+
+            }).catch((error) => reject(error));
+        })
+    }
 }
